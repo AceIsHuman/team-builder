de8e61dc2b4f1886e5b93e080ebaf2375b23ba75
AceIsHuman <menoman1995@yahoo.com>
2019-08-15 12:18:10 -0500

    add handleChange function

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -7,16 +7,20 @@ import React, { useState } from 'react'
 function Form() {
     const [formData, setFormData] = useState({ name: '', email: '', role: '' })
 
+    const handleChange = event => {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
+
     return (
         <form>
             <label for="name">Name</label>
-            <input type="text" name="name" value={formData.name}/>
+            <input type="text" name="name" value={formData.name} onChange={handleChange} />
 
             <label for="email">Email</label>
-            <input type="text" name="email" value={formData.email}/>
+            <input type="text" name="email" value={formData.email} onChange={handleChange} />
 
             <label for="role">Role</label>
-            <input type="text" name="role" value={formData.role} />
+            <input type="text" name="role" value={formData.role} onChange={handleChange} />
         </form>
     )
 }
